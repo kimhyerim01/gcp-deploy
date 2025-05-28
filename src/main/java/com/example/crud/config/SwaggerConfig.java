612bc084d8510f2526fadc15ba1f9e4c67eaf789
김혜림 <khrim981209@gmail.com>
2025-05-28 20:51:57 +0900

    .

---
+++ src/main/java/com/example/crud/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.example.crud.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .version("v1.0") //버전
+                .title("KCAMP-10 API"); //이름
+        return new OpenAPI()
+                .info(info);
+    }
+
+}
